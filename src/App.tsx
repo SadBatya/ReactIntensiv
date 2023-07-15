3f65d51eece56227a16a9ea903b42293725fb11a
sadbatya <sadbatya@mail.ru>
2023-07-15 16:13:19 +0200

    feat ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
- import Product from './components/Product'
- import { products } from './data/products'
+import Product from './components/Product';
+import { useProducts } from './hooks/products';
+
 
 function App() {
+  const { loading, error, products } = useProducts()
   return (
     <div className='container mx-auto max-w-2xl pt-5'>
-      { products.map(product => <Product product={product} />)}
+      { loading && <p className='text-center'>Loading...</p> }
+      { error && <p className='text-center text-red-600'>{ error }</p> }
+      { products.map((product) => (<Product product={product} key={product.id} /> )) }
     </div>
-  )
+  );
 }
 
 export default App;
